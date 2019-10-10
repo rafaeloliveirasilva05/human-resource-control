4adc4bcd7337887bf8c5ff1a8fe467e3ef598d92
rafael05 <rafaeloliveirasilva05@gmail.com>
2019-10-10 12:01:24 -0300

    header structure development

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { FaUser } from 'react-icons/fa';
+
+import './styles.css'
 
 function Header() {
-  return(
-    <div>
-      minha header
-    </div>
+  return (
+    <header>
+      <div className='container'>
+        <div className='userData'>
+          <label>Name</label>
+          <div>
+            <FaUser size={26} color={'#a9a9a9'} />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/screens/PresentationEmployeeData/index.js
+++ src/screens/PresentationEmployeeData/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function PresentationEmployeeData (){
   return(
     <div>
-      oi
+      teste
     </div>
   )
 }
